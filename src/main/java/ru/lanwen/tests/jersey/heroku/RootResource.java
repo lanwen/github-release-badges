fcee6c669a4777909117b705e93cac01a6d66877
MerkushevKirill <lanwen@yandex-team.ru>
2014-06-23 01:37:03 +0400

    add - get github version

--- src/main/java/ru/lanwen/tests/jersey/heroku/RootResource.java
+++ src/main/java/ru/lanwen/tests/jersey/heroku/RootResource.java
@@ -1,25 +1,68 @@
 package ru.lanwen.tests.jersey.heroku;
 
+import org.glassfish.jersey.internal.util.Base64;
+import ru.lanwen.tests.jersey.heroku.misc.Defaults;
+
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import java.net.URI;
 
-/**
- * Root resource (exposed at "myresource" path)
- */
-@Path("/")
+import static com.jayway.jsonpath.JsonPath.read;
+import static java.lang.String.format;
+import static javax.ws.rs.core.UriBuilder.fromUri;
+
+@Path("/github/{owner}/{repo}/badge.{format}")
 public class RootResource {
 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Hello, Heroku!";
+    public Response getIt(@PathParam("owner") String owner,
+                          @PathParam("repo") String repo,
+                          @PathParam("format") String format,
+                          @QueryParam("style") @DefaultValue("default") String style) {
+
+        Client client = ClientBuilder.newClient();
+
+        Response github = client
+                .target(fromUri(Defaults.GITHUB_BASE_URI)
+                        .path("/repos/{owner}/{repo}/releases").build(owner, repo))
+                .request()
+                .header(HttpHeaders.AUTHORIZATION, basicWith(Defaults.TOKEN))
+                .header(HttpHeaders.USER_AGENT, Defaults.USER_AGENT_VALUE)
+                .get();
+
+        String releases = github.readEntity(String.class);
+
+        String release = read(releases, "$.[0].tag_name");
+        boolean isPrerelease = read(releases, "$.[0].prerelease");
+
+        URI shieldUri = fromUri(Defaults.SHIELDS_IO_BASE_URI)
+                .path("/badge/release-{status}-{color}.{format}")
+                .queryParam("style", style)
+                .build(
+                        escape(release),
+                        isPrerelease ? Defaults.ORANGE_COLOR : Defaults.GREEN_COLOR,
+                        format
+                );
+
+        System.out.println(format("[Remain: %s] %s",
+                github.getHeaderString("X-RateLimit-Remaining"),
+                shieldUri));
+
+        return client.target(shieldUri).request().get();
+    }
+
+    private String escape(String release) {
+        return release.replaceAll("-", "--");
+    }
+
+    private String basicWith(String token) {
+        return format("Basic %s", Base64.encodeAsString(token + ":x-oauth-basic"));
     }
 }
---
+++ src/main/java/ru/lanwen/tests/jersey/heroku/misc/Defaults.java
@@ -0,0 +1,25 @@
+package ru.lanwen.tests.jersey.heroku.misc;
+
+/**
+ * User: lanwen
+ * Date: 23.06.14
+ * Time: 0:58
+ */
+public final class Defaults {
+
+    public static final String TOKEN = "5a379df57152150853751fbab63809d03dbd8661";
+
+
+    public static final String GITHUB_BASE_URI = "https://api.github.com";
+    public static final String SHIELDS_IO_BASE_URI = "http://img.shields.io";
+
+
+    public static final String USER_AGENT_VALUE = "Lanwen GitHub repo checker";
+
+    public static final String ORANGE_COLOR = "orange";
+    public static final String GREEN_COLOR = "brightgreen";
+
+    private Defaults() { }
+
+
+}
